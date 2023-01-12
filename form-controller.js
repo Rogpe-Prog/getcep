121accb3bd7de4663b52eb8757d2d3b17423d242
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-01-12 07:25:43 -0300

    create a card

--- form-controller.js
+++ form-controller.js
@@ -1,5 +1,6 @@
 import Address from './address.js';
 import * as addressService from './address-service.js'
+import * as listController from './list-controller.js';
 
 function State() {
     this.address = new Address();
@@ -69,8 +70,7 @@ async function handleInputCepChange(event) {
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    
-    console.log(state.address);
+    listController.addCard(state.address);
 }
 
 function handleInputNumberChange(event) {
--- getcep.js
+++ getcep.js
@@ -1,3 +1,5 @@
 import * as formController from './form-controller.js';
+import * as listController from './list-controller.js';
 
 formController.init();
+listController.init();
