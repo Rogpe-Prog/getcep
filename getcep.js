6c30352076924da549b1bc0dd63fd25eaa1c072b
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-01-16 08:05:47 -0300

    finish first version

--- getcep.js
+++ getcep.js
@@ -1,5 +1,9 @@
 import * as formController from './form-controller.js';
 import * as listController from './list-controller.js';
+import * as modalController from './modal-controller.js';
+import * as pageController from './page-controller.js';
 
 formController.init();
 listController.init();
+modalController.init();
+pageController.init();
